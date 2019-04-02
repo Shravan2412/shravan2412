dc52282e4456549055430e07a5cacca1f5b4b6f0
sthillaiarasu <thillaiarasu.s@gmail.com>
2019-04-02 11:50:19 +0530

    resend edited

--- src/demo/Resend.java
+++ src/demo/Resend.java
@@ -1,7 +1,18 @@
 package demo;
 
+
+
 public class Resend {
+	public void method()
+	{
+		System.out.println("hi hello");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("hi hello");
+		
+		Resend rd=new Resend();
+		rd.method();
+	
 	}
 }
